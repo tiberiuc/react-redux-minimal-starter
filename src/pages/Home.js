60027302dd9c33654c74ac14587fa4f69b23fd66
Tiberiu Craciun <tibi@happysoft.ro>
2015-10-16 23:16:38 +0300

    Revert "Added ES5 example"
    This reverts commit fcc96d5864142e93aea9d7237b5cd504785ad774.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import SmartComponent from '../components/SmartComponent'
-import SmartComponentES5 from '../components/SmartComponentES5'
 import DumbComponent from '../components/DumbComponent'
 
 class Home extends React.Component {
@@ -15,7 +14,7 @@ class Home extends React.Component {
     const {state, dispatch} = this.props
     return (
       <div>
-        <SmartComponentES5 />
+        <SmartComponent />
         <DumbComponent />
       </div>
     )
